01abd2b13a1e919ad021d35251dec49cd330a791
erind <erindmehmeti@gmail.com>
2023-02-13 10:27:47 +0100

    more changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  includeLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+
+  password = '';
+  length = 0;
+
+
+
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@##$%^&*()';
+
+    let validChars ='';
+
+    if (this.includeLetters) {
+      validChars += letters;
+       
+    } 
+    if (this.useNumbers){
+      validChars += numbers;
+    }
+
+    if (this.useSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i=0; i<this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+
+
+  }
+
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeLength(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+
+    const parsedValue = parseInt(filterValue);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+  
 }
